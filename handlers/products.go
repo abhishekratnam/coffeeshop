3e154bf67f4d4ae8f840e3b4776af7b6f4fa4efc
root <anshu98@outlook.com>
2021-02-01 16:51:39 +0530

    Restful services

--- handlers/products.go
+++ handlers/products.go
@@ -18,12 +18,27 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		p.getProducts(rw, r)
 		return
 	}
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 func (p *Products) getProducts(rw http.ResponseWriter, h *http.Request) {
+	p.l.Println("Handle GET Product")
 	lp := data.GetProducts()
 	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshall json", http.StatusInternalServerError)
 	}
+}
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Product")
+
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshall json", http.StatusBadRequest)
+	}
+	data.AddProduct(prod)
 }
